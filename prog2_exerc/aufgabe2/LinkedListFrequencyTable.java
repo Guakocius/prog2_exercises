cc63f47af0734f8d02b492694fac45f3572eb193
Timothy T. <timo-drexler@online.de>
2025-04-01 10:22:49 +0200

    replaced b1, e1 to begin, end

--- prog2_exerc/aufgabe2/LinkedListFrequencyTable.java
+++ prog2_exerc/aufgabe2/LinkedListFrequencyTable.java
@@ -19,7 +19,7 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
             this.next = n;
         }
     }
-    private Node begin, end, b1, e1;
+    private Node begin, end;
 
     public LinkedListFrequencyTable() {
         clear();
@@ -32,8 +32,8 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
 
     @Override
     public final void clear() {
-        b1 = new Node(null, e1, null);
-        e1 = new Node(null, null, b1);
+        begin = new Node(null, end, null);
+        end = new Node(null, null, begin);
         size = 0;
     }
 
@@ -42,33 +42,28 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
         Word wo = new Word(w, f);
 
         if (size == 0) {
-            Node r = new Node(wo, e1, b1);
-            b1.next = r;
-            e1.prev = r;
+            Node r = new Node(wo, end, begin);
+            begin.next = r;
+            end.prev = r;
             size++;
         } else {
-            Node e = e1.prev;
+            Node e = end.prev;
             boolean freqAdd = false;
             for (int i = size - 1; i >= 0; i--) {
-                // System.out.println("In for add()-LOOP");
                 if (e.word.getWord().equals(w)) {
                     e.word.addFrequency(f);
-                    // System.out.println("adding f & sorting in add()");
                     sort(e);
-                    // System.out.println("End sort(e)");
                     freqAdd = true;
                     break;
                 }
                 e = e.prev;
             }
             if (freqAdd == false) {
-                Node r = new Node(wo, e1, e1.prev);
+                Node r = new Node(wo, end, end.prev);
 
-                e1.prev.next = r;
-                // System.out.println("prev: " + e1.prev.word.getWord());
-                e1.prev = r;
+                end.prev.next = r;
+                end.prev = r;
                 size++;
-                // System.out.println("Sort r...");
                 sort(r);
             }
         }
@@ -77,7 +72,7 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
     @Override
     public Word get(int pos) {
         if (pos + 1 > size) return null;
-        Node n = b1.next;
+        Node n = begin.next;
         for (int i = 0; i < size; i++) {
             if (i == pos) {
                 return n.word;
@@ -87,16 +82,15 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
         }
         if (pos >= size) return null;
         int counter = size() - 1;
-        for (Node e = e1.prev; !e.equals(b1); e = e.prev) {
+        for (Node e = end.prev; !e.equals(begin); e = e.prev) {
             if (counter == pos) return e.word;
             counter--;
         }
-        // System.out.println("Word not found");
         return null;
     }
     @Override
     public int get(String w) {
-        Node n = b1.next;
+        Node n = begin.next;
 
         for (int i = 0; i < size(); i++) {
             if (n.word.getWord().equals(w)) {
@@ -112,16 +106,13 @@ public class LinkedListFrequencyTable extends AbstractFrequencyTable {
         int freq = n.word.getFrequency();
         Word t = n.word;
         Node p = n.prev;
-        while (!p.equals(b1) && p.word.getFrequency() < freq) {
-            // System.out.println("sorting...");
-            // System.out.println("Setting:" + p.next.word.getWord() + " to " + p.word.getWord());
+        while (!p.equals(begin) && p.word.getFrequency() < freq) {
             p.next.word = p.word;
             p = p.prev;
 
         }
-        if (p.equals(b1)) {
-            // System.out.println("replace " + b1.next.word.getWord() +" with: " + t);
-            b1.next.word = t;
+        if (p.equals(begin)) {
+            begin.next.word = t;
         } else {
             p.next.word = t;
         }
