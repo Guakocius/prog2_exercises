bedf92fb21ec3da472ea3df9fffd91d10ff5c190
Guakocius <engelhardt.alexander@mein.gmx>
2025-04-22 12:26:26 +0200

    Added main and recursive methods and delta angle to v1 and v2

--- prog2_exerc/aufgabe5/PythagorasTree_V1.java
+++ prog2_exerc/aufgabe5/PythagorasTree_V1.java
@@ -1,5 +1,26 @@
 package aufgabe5;
 
+import java.lang.Math;
+
 public class PythagorasTree_V1 {
-    
+
+    private final int DELTA = 30;
+
+    /*
+     * The main method is the entry point of the program.
+     * @param args not used
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    /*
+     * Draws a Pythagorean tree based on the given angle DELTA.
+     */
+    public static void drawPyTree() {
+        
+
+    }
+
 }
