78c62c1984f27affe5d271c10a9ee72fba01a941
Timothy <timo-drexler@online.de>
2025-04-22 11:23:29 +0200

    finished a43

---
+++ prog2_exerc/aufgabe4/a43/a43/AbstractFrequencyTable.java
@@ -0,0 +1,61 @@
+package a43;
+
+/**
+ *
+ * @author Alexander Engelhardt
+ * @author Timothy Drexler
+ */
+
+public abstract class AbstractFrequencyTable<T> implements FrequencyTable<T> {
+	@Override
+	public boolean isEmpty() {
+		return this.size() == 0;
+	}
+
+	@Override
+    public void add(T w) {
+		add(w, 1);
+    }
+
+	@Override
+	public void addAll(FrequencyTable<? extends T> fq) {
+        if (!fq.isEmpty()) {
+            // for (int i = 0; i < fq.size(); i++) {
+            //     this.add(fq.get(i).getWord(), fq.get(i).getFrequency());
+            // }
+            for (Word<? extends T> wo : fq) {
+                this.add(wo.getWord(), wo.getFrequency());
+            }
+        }
+	}
+
+	@Override
+	public void collectNMostFrequent(int n, FrequencyTable<? super T> fq) {
+        fq.clear();
+        if (this.size() < n) {
+            fq.addAll(this);
+        } else {
+            // for (int i = 0; i < n; i++) {
+            //     fq.add(this.get(i).getWord(), this.get(i).getFrequency());
+            // }
+            for (int i = 0; i < n; i++){
+                Word<? extends T> wo = this.get(i);
+                fq.add(wo.getWord(), wo.getFrequency());
+            } 
+        }
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder("{");
+		// Ihr Code:
+
+        for (int i = 0; i < this.size(); i++) {
+            String w = this.get(i).toString();
+            s.append(w);
+            s.append(", ");
+        }
+        s.append("} size = ").append(this.size());
+		return s.toString();
+    }
+}
---
+++ prog2_exerc/aufgabe4/a43/a43/a43/Word.java
@@ -0,0 +1,46 @@
+package a43;
+
+/**
+ * Klasse für Wörter mit ihren Häufigkeiten.
+ * 
+ * @author Alexander Engelhardt
+ * @author Timothy Drexler
+ */
+public class Word {
+	private final String word;
+	private int frequency;
+
+	/**
+	 * Konstruktor.
+	 * @param word Wort
+	 * @param f H&auml;ufigkeita
+	 */
+	public Word(String word, int f) {
+        this.word = word;
+		this.frequency = f;
+	}
+
+    	/**
+	 * Liefert Wort zur&uuml;ck.
+	 * @return Wort
+	 */
+	public String getWord() {
+		return word;
+	}
+
+	/**
+	 * Liefert H&auml;ufgkeit zur&uuml;ck.
+	 * @return H&auml;ufgkeit
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+
+	/**
+	 * Addiert zur H&auml;ufgkeit f dazu.
+	 * @param f H&auml;ufgkeits&auml;nderung.
+	 */
+	public void addFrequency(int f) {
+		frequency += f;
+	}
+}
